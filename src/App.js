46e444cb7d7503cdde73f05707bfc3610322fb2b
Benjamin Munoz-Palma <bomunoz@uc.cl>
2025-08-02 15:57:57 -0400

    Passing inputs, props, into squares

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+function Square({value}) { // value is considered a prop as it will be passed 
+  return <button className="square">{value}</button>; // {} are relevant
+}
+
+
+
 // export: makes this component available for import in other files
 // default: this is the default export of the file, so it can be imported without braces
 // return statement uses JSX: syntax extension that looks like HTML but compiles to React.createElement
@@ -8,25 +14,24 @@ export default function Board() {
 
   // you’ll give each square a number to make sure you know where each square is displayed
   // The CSS defined in styles.css styles the divs with the className of board-row.
-    return (
-    <>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
-      </div>
-
-      <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
-    </>
-  );
+  return (
+      <>
+        <div className="board-row">
+          <Square value="1" />
+          <Square value="2" />
+          <Square value="3" />
+        </div>
+        <div className="board-row">
+          <Square value="4" />
+          <Square value="5" />
+          <Square value="6" />
+        </div>
+        <div className="board-row">
+          <Square value="7" />
+          <Square value="8" />
+          <Square value="9" />
+        </div>
+      </>
+    );
 }
 
